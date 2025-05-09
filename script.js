77facd220bca4532800eafa5cd2f021e0cf76cd8
Penuel Zechariah James <penuelzechariah4545@gmail.com>
2025-05-09 16:18:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fade-in effect on scroll
+
 window.addEventListener('scroll', function () {
     document.querySelectorAll('.content').forEach(box => {
         if (box.getBoundingClientRect().top < window.innerHeight - 50) {
@@ -7,7 +7,7 @@ window.addEventListener('scroll', function () {
     });
 });
 
-// FAQ toggle animation
+
 document.querySelectorAll('.faq h3').forEach(faq => {
     faq.addEventListener('click', function () {
         this.parentElement.classList.toggle('open');
@@ -15,19 +15,19 @@ document.querySelectorAll('.faq h3').forEach(faq => {
     });
 });
 
-// Intersection Observer to observe elements and add 'visible' class when they come into view
+
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-            observer.unobserve(entry.target); // Stop observing the element once it becomes visible
+            observer.unobserve(entry.target); 
         }
     });
 }, {
-    threshold: 0.5 // Trigger when 50% of the element is in view
+    threshold: 0.5 
 });
 
-// Target elements to observe
+
 const sections = document.querySelectorAll('section');
 sections.forEach(section => {
     observer.observe(section);
